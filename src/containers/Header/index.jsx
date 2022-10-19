4c4a3cdac352a2ce206ad3879f51b14e8b333808
JairoDuenas <jairoduenas.ing@gmail.com>
2022-10-18 21:29:33 -0500

    Agregando información peronal y ajustando estilos

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Container, Toolbar } from '@mui/material';
+import { AppBar, Box, Container, Toolbar } from '@mui/material';
 import React from 'react'
 import Logo from '../../components/Logo';
 import MenuHeader from '../../components/MenuHeader';
@@ -8,8 +8,18 @@ import NameLogo from '../../components/NameLogo';
 export default function Header() {
 
   return (
-    <AppBar position='fixed' sx={{ p: 0.2, background: '#212121'}}>
-      <Container maxWidth='xl' sx={{p: 0}}>
+    <AppBar
+      className='app-bar'
+      position='fixed'
+      sx={{
+        p: 0.2,
+        backgroundColor: '#212121',
+      }}
+    >
+      <Container
+        maxWidth='xl'
+        sx={{ p: 0 }}
+      >
         <Toolbar sx={{px: 1}}>
           <MenuIconHeader  />
           <Logo />
--- src/routes/Homepage/index.jsx
+++ src/routes/Homepage/index.jsx
@@ -1,10 +1,16 @@
+import { Box, Container, Stack } from '@mui/material';
 import React from 'react'
 import Header from '../../containers/Header';
+import MainInfo from '../../components/MainInfo';
 
 export default function HomePage() {
   return (
-    <>
-      <Header>header</Header>
-    </>
+    <Container
+      className='main-container'
+      sx={{px: '5px'}}
+    >
+      <Header />
+      <MainInfo />
+    </Container>
   )
 }
